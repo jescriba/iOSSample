cbaabee7ed16dc9415cf60f233bbef6ddaad6fef
Joshua Escribano <escribirajoshua@gmail.com>
2016-02-09 13:47:51 -0800

    Adds webview and scales it to page

--- NativeSampleApp/WebViewController.swift
+++ NativeSampleApp/WebViewController.swift
@@ -6,7 +6,7 @@ class WebViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        webView?.loadRequest(NSURLRequest(URL: NSURL(string: "https://google.com")!))
+        webView!.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("SampleHtml", ofType:"html")!)))
         // Do any additional setup after loading the view, typically from a nib.
     }
     
